b4b0d913ae37f97e9014990b12dfa3c4fd5c4b9d
Rahul Kumar Singh <rs3448397@gmail.com>
2025-04-13 19:22:27 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
